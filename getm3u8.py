41729cee503bc010b1577ba9736985f5d6558dfb
Jueming Feng <jueming@Juemings-MacBook-Pro.local>
2020-09-16 19:46:56 +0900

    force 1080

--- getm3u8.py
+++ getm3u8.py
@@ -2,9 +2,11 @@
 url = 'https://m-epg.cmishow.com:1443/?s=104&p=mLiveChannel&k=1&v=1&c=1&a=852&i=3&channelId=1568275619470108006&l=zh_TW'
 import requests
 import json
+import re
 response = requests.get(url)
 data = json.loads(response.text)
 m3u8 = data['data']['detail']['livePlayurls'][0]['playurl']
+m3u8 = re.sub(".m3u8",r'-3.m3u8', m3u8)
 f = open("icable.m3u8", 'w')
 f.write("#EXTM3U"+'\n')
 f.write("#EXT-X-VERSION:6"+"\n")
